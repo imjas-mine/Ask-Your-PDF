b351352f29d1f84179b78a3713fe716ca710373d
jasmine <116243241+imjas-mine@users.noreply.github.com>
2025-09-28 00:15:07 -0400

    implementing the retrievel from the llm and displaying the response on the screen

--- app.py
+++ app.py
@@ -5,42 +5,50 @@ from PyPDF2 import PdfReader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask Your PDF")
     st.header("Ask Your PDF")
-    
-    # uploading the file
-    pdf=st.file_uploader("Upload your PDF", type="pdf")
-    
-    #extract the text
-    if pdf is not None:
-        pdf_reader=PdfReader(pdf)
-        text=""
-        for page in pdf_reader.pages:
-            text+=page.extract_text()
-        
-        #split into chunks
-        text_splitter=CharacterTextSplitter(
-            separator="\n",
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks=text_splitter.split_text(text)
-        
-        
-        #creating embeddings
-        embeddings=OpenAIEmbeddings()
-        knowledge_base=FAISS.from_texts(chunks,embeddings)
-        
-        user_ques=st.text_input("Ask a question from your pdf")
-        if user_ques:
-            docs=knowledge_base.similarity_search(user_ques)
-            st.write(docs)
-        
 
-if __name__=='__main__':
+    # uploading the file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    # extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+        # creating embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        user_ques = st.text_input("Ask a question from your pdf")
+        if user_ques:
+            retriever = knowledge_base.as_retriever()
+            llm = ChatOpenAI(model="gpt-4o-mini")
+
+            prompt = ChatPromptTemplate.from_template(
+                "Use the following context to answer the question.\n\nContext:\n{context}\n\nQuestion: {input}"
+            )
+            document_chain = create_stuff_documents_chain(llm,prompt)
+            retrieval_chain = create_retrieval_chain(retriever, document_chain)
+            response = retrieval_chain.invoke({"input": user_ques})
+            st.write(response["answer"])
+
+
+if __name__ == "__main__":
     main()
